d30dc47a37d8e34696c047c0c3fa1333942cd15f
Ryan Chen <94326100+Ryxn7@users.noreply.github.com>
2022-11-21 00:25:14 -0500

    v1.8

--- cogs/admin.py
+++ cogs/admin.py
@@ -12,14 +12,16 @@ class admin(commands.Cog):
 
     def __init__(self,client):
         self.client = client
+    
 
     @commands.Cog.listener()
     async def on_ready(self):
-        activity = discord.Game(name = f"bd.help")
+        activity = discord.Game(name = f"/help")
         await self.client.change_presence(status=discord.Status.online, activity=activity)
         logger.info("Birthday Bot is ready.")
-    
-    @commands.command(aliases=["h"])
+
+
+    @discord.slash_command(name="help", description="Here's the help prompt!")
     async def help(self, ctx):
         user = ctx.author
         embed = discord.Embed(
@@ -35,32 +37,35 @@ class admin(commands.Cog):
 
         embed.add_field(
             name = "Commands List",
-            value = "`bd.add (month) (day) @user` - Command to add a birthday to the database." \
-                    "\n> Aliases: `bd.a (month)`" \
-                    "\n> Example usages: `bd.add 06 02 @user`"
+            value = "`/add (month) (day) @user` - Command to add a birthday to the database."
+                    "\n> Example usage: `/add 06 02 @user`"
 
-                    "\n\n`bd.countdown @user`" \
-                    "\n> Returns the birthdays that have been added to this month." \
-                    "\n> Aliases: `bd.c @user`"
+                    "\n\n`/countdown @user`" \
+                    "\n> Returns the birthdays that have been added to this month."
 
-                    "\n\n`bd.? @user`" \
+                    "\n\n`/?when @user`" \
                     "\n> Returns user's birthday." \
 
-                    "\n\n`bd.invite`" \
-                    "\n> Returns Birthday Bot's Invite Link." \
-                    "\n> Aliases: `bd.i`",
+                    "\n\n`/invite`" \
+                    "\n> Returns Birthday Bot's Invite Link.",
             inline = False
         )
 
         embed.set_footer(text="By Ryxn and Pancreas <3")
-        await ctx.send(embed=embed)
-    
+        await ctx.respond(embed=embed)
 
-    @commands.command(aliases = ["i"])
+
+    @discord.slash_command(name="invite", description="Bot's invite link!")
     async def invite(self, ctx):
         invite_link = database["invite_link"]
-        await ctx.reply(f"Add Birthday Bot to your own server using this [link]({invite_link})!")
-    
+        embed = discord.Embed(title="Birthday Bot | Invite Link | :confetti_ball:", color=0xFF69B4)
+        embed.add_field(
+            name = "Add Birthday Bot to your own server down below!",
+            value = f"[Birthday-Bot]({invite_link})!",
+            inline = False
+        )
+        await ctx.respond(embed=embed)
+
 
 def setup(client):
     client.add_cog(admin(client))
